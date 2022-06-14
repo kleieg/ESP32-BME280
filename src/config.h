f246eeccb7365c2e9786f6f9cd5ca5ac05525e74
kleieg <egbert.kleinhans@gmail.com>
2022-06-14 18:28:23 +0200

    first version; copied from Kellerfenster

--- src/config.h
+++ src/config.h
@@ -1,13 +1,15 @@
 // set hostname used for MQTT tag and WiFi
-#define HOSTNAME "ESP-BME280"
+#define HOSTNAME "ESP-BME280-2"
 #define MQTT_BROKER "192.168.178.15"
-#define VERSION "v 1.0.0"
+#define VERSION "v 0.9.0"
 
 #define MQTT_INTERVAL 120000
 #define RECONNECT_INTERVAL 5000
 
 #if ARDUHAL_LOG_LEVEL >= ARDUHAL_LOG_LEVEL_INFO
 #define SERIALINIT Serial.begin(115200);
+#define LEDBLINK int espLed = 5;
 #else
 #define SERIALINIT
+#define LEDBLINK int espLed = 1;
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -41,14 +41,6 @@ Adafruit_BME280 bme; // I2C
 /*
 used GPIOs 
 BMNE280: SDA = 21  SCL = 22
-
-Fensterschalter = 18
-Taster = 19
-
-Motorsteuerung = 16 & 17
-
-LEDs  25 = auto  26 = open  27 = close
-
 */
 // variables for BME280
 bool status;
@@ -59,33 +51,11 @@ float BME_Humidity;
 float BME_Pressure;
 long  BME_Time;
 
-// mode set by IP-Symcon or Button
-int Mode = 1;   // 1 = auto  2 = open  3 = closed
 
 // variables for LED 
-int LED_auto = 25;
-int LED_open = 26;
-int LED_close = 27;
 bool led = 1;
-
-// Fensterkontakt
-bool WindowState;
-int WindowGpio = 18;
-int Window_scanIntervall = 1000;  // in milliseconds
-long WindowlastScan = 0;
-
-// Taster
-int ButtonGpio = 19;
-int Button_scanIntervall = 1000;  // in milliseconds
-long ButtonlastScan = -1000;
-
-// Motor Fenster
-bool ButtonState;
-int OpenGpio = 16;
-int CloseGpio = 17;
-int MotorDuration = 15000; //  in milliseconds
-long MotorStart = 0;
-
+long LEDblink = 0;
+LEDBLINK
 
 
 // Create AsyncWebServer object on port 80
@@ -118,12 +88,11 @@ String getOutputStates(){
   myArray["cards"][3]["c_text"] = String(MQTT_INTERVAL) + "ms";
   myArray["cards"][4]["c_text"] = String(U_days) + " days " + String(U_hours) + ":" + String(U_min) + ":" + String(U_sec);
   myArray["cards"][5]["c_text"] = "WiFi = " + String(WiFi_reconnect) + "   MQTT = " + String(Mqtt_reconnect);
-  myArray["cards"][6]["c_text"] = String(Mode);
+  myArray["cards"][6]["c_text"] = " ";
   myArray["cards"][7]["c_text"] = " to reboot click ok";
   myArray["cards"][8]["c_text"] = String(BME_Humidity) +"%";
   myArray["cards"][9]["c_text"] = String(BME_Pressure) ;
   myArray["cards"][10]["c_text"] = String(BME_Temperature) + "Grad";
-  myArray["cards"][11]["c_text"] = String(Mode);
   
   String jsonString = JSON.stringify(myArray);
   log_i("%s",jsonString.c_str()); 
@@ -181,93 +150,6 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,AwsEventType t
   }
 }
 
-
-// LED blink 
-void LED_blink(int stat) {
-    
-    if (stat == 0) {
-      if(led == 0){
-        led = 1;
-      }else{
-        led = 0;
-      }
-    }else{
-      led = 1;
-    }
-
-    switch (Mode) {
-      case 1:
-        digitalWrite(LED_auto, led);
-        digitalWrite(LED_open, 0);
-        digitalWrite(LED_close, 0);
-      break;
-      case 2:
-        digitalWrite(LED_auto, 0);
-        digitalWrite(LED_open, led);
-        digitalWrite(LED_close, 0);
-      break;
-      case 3:
-        digitalWrite(LED_auto, 0);
-        digitalWrite(LED_open, 0);
-        digitalWrite(LED_close, led);
-      break;
-    }
-  }   
-
-// receive MQTT messages
-void MQTT_callback(char* topic, byte* message, unsigned int length) {
-  
-  log_i("%s","Message arrived on topic: ");
-  log_i("%s\n",topic);
-  log_i("%s","Data : ");
-
-  String MQTT_message;
-  for (int i = 0; i < length; i++) {
-    MQTT_message += (char)message[i];
-  }
-  log_i("%s\n",MQTT_message);
-
-  String windowTopic = Hostname + "/CMD/Window";
-  String modeTopic = Hostname + "/CMD/Mode";
-  String strTopic = String(topic);
-
-  if (strTopic == windowTopic ){
-
-    LED_blink(1);
-    ButtonlastScan = ButtonlastScan + MotorDuration;       // ignore pressed botton next MotorDuration seconds
-
-    if(MQTT_message == "1"){
-      log_i("%s\n","open window");
-      digitalWrite(OpenGpio, LOW);
-      MotorStart = millis();
-    }
-    else if(MQTT_message == "2"){
-      log_i("%s\n","close window");
-      digitalWrite(CloseGpio, LOW);
-      MotorStart = millis();
-    }
-  }
-
-  if (strTopic == modeTopic ){
-
-    if(MQTT_message == "1"){
-      log_i("%s\n","Mode = auto");
-      Mode = 1;
-    }
-    if(MQTT_message == "2"){
-      log_i("%s\n","Mode = open");
-      Mode = 2;
-    }
-    if(MQTT_message == "3"){
-      log_i("%s\n","Mode = close");
-      Mode = 3;
-    }
-  }
-  notifyClients(getOutputStates());
-  Mqtt_lastSend = now - MQTT_INTERVAL - 10;  // --> MQTT send !!
-
-}
-
 // init BME280
 void setup_BME280() {
   // default settings
@@ -279,7 +161,6 @@ void setup_BME280() {
   }
   log_i("%s\n","BME280 sensor connected");
 }
-
 
 
 // read BME280 values
@@ -306,20 +187,9 @@ void MQTTsend () {
   mqtt_data["Hum"] = round (BME_Humidity*10) / 10;
   mqtt_data["Pres"] = round (BME_Pressure*10) / 10;
 
-  actuators["Mode"] = Mode;
-   if  ( WindowState ) {
-    actuators["Window"] = 1;
-  }
-  else{
-    actuators["Window"] = 0;
-  }
-  mqtt_data["Actuators"] = actuators;
-
-
   String mqtt_string = JSON.stringify(mqtt_data);
 
   log_i("%s\n", mqtt_string.c_str());
-
 
   Mqttclient.publish(mqtt_tag.c_str(), mqtt_string.c_str());
 
@@ -334,30 +204,19 @@ void setup() {
   log_i("setup device\n");
 
   // initialise LED
-  pinMode(LED_auto, OUTPUT);
-  pinMode(LED_open, OUTPUT);
-  pinMode(LED_close, OUTPUT);
+  pinMode(espLed, OUTPUT);
 
-  // initialise Fensterkontakt, Taster  und Motor
-  pinMode(WindowGpio, INPUT);
-  pinMode(ButtonGpio, INPUT);
-  pinMode(OpenGpio, OUTPUT);
-  pinMode(CloseGpio, OUTPUT);
-  digitalWrite (OpenGpio, HIGH);
-  digitalWrite (CloseGpio, HIGH);
 
   log_i("setup WiFi\n");
   initWiFi();
 
   log_i("setup MQTT\n");
   Mqttclient.setServer(MQTT_BROKER, 1883);
-  Mqttclient.setCallback(MQTT_callback);
 
   log_i("setup BME280\n");
   setup_BME280() ;
   delay(10);
   BME280_scan();
-
 
   initSPIFFS();
  
@@ -397,50 +256,23 @@ void loop() {
 
   now = millis();
 
+  // LED blink
+  if (now - LEDblink > 200) {
+    LEDblink = now;
+    if(led == 0) {
+      digitalWrite(espLed, 1);  // LED aus
+      led = 1;
+    }else{
+      digitalWrite(espLed, 0);  // LED ein
+      led = 0;
+    }
+  }
 
   // perform BME280 scan  
    if (now - BME280lastScan > BME280_scanIntervall) {
     BME280lastScan = now;
     BME280_scan();
-  }    
-
-    // check Fensterkontakt
-   if (now - WindowlastScan > Window_scanIntervall) {
-    WindowlastScan = now;
-    
-    bool OldState = WindowState;
-    WindowState = digitalRead(WindowGpio);
-    if ((WindowState == LOW) && (OldState == HIGH)){
-      MotorStart = 0;
-      digitalWrite (CloseGpio, HIGH);             // window closed. switch off motor
-      Mqtt_lastSend = now - MQTT_INTERVAL - 10;  // --> MQTT send !!
-    }
-    if ((WindowState == HIGH) && (OldState == LOW)){
-      Mqtt_lastSend = now - MQTT_INTERVAL - 10;  // --> MQTT send !!
-    }
-  }    
-
-    // check Taster
-   if (now - ButtonlastScan > Button_scanIntervall) {
-    ButtonlastScan = now;
-    LED_blink(0);
-    ButtonState = digitalRead(ButtonGpio);
-    if (ButtonState == LOW) {
-      Mode = Mode + 1;
-      if ( Mode == 4 ) Mode = 1;
-      Mqtt_lastSend = now - MQTT_INTERVAL - 10;  // --> MQTT send !!
-      ButtonlastScan = ButtonlastScan + 2000;       // ignore pressed botton next 2 seconds
-      LED_blink(1);
-    }
-  } 
-  
-  
-  // check Motor
-   if ((now - MotorStart > MotorDuration) && (MotorStart != 0 )){
-      MotorStart = 0;
-      digitalWrite (OpenGpio, HIGH);
-      digitalWrite (CloseGpio, HIGH);
-  }    
+  }     
 
   // check WiFi
   if (WiFi.status() != WL_CONNECTED  ) {
